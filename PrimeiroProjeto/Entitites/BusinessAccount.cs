710c39603c82b8bdcbc13631e56ef9aac38071dc
Iago Lima <iagouruha@outlook.com>
2023-09-27 10:23:13 -0300

    Adcionado emprestimo

--- PrimeiroProjeto/Entitites/BusinessAccount.cs
+++ PrimeiroProjeto/Entitites/BusinessAccount.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace PrimeiroProjeto.Entitites
 {
-    internal class BusinessAccount
+    class BusinessAccount : Account
     {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() 
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanlimit) 
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanlimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+
     }
 }
